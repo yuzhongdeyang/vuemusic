6b43d2cea59be823148e98d542f5e4f6eb32b06f
guoyt <1281738893@qq.com>
2023-05-26 13:32:00 +0800

    2023-5-26

--- musicobj02/src/api/index.js
+++ musicobj02/src/api/index.js
@@ -4,6 +4,10 @@ const baseUrl = "http://localhost:3000";
 export function getBanner(type=0){
     return axios.get(`${baseUrl}/banner?type=${type}`);
 }
+//封装获取歌单的api  调用例子 ： personalized?limit=30 limit=10,50,100
+export function getMusic(limit=10){
+    return axios.get(`${baseUrl}/personalized?limit=${limit}`);
+}
 
 //对外抛出
-export default {getBanner};
+export default {getBanner,getMusic};
--- musicobj02/src/components/IconList.vue
+++ musicobj02/src/components/IconList.vue
@@ -1,6 +1,29 @@
 <template>
-    <div>
-        我是lonlist
+    <div class="iconList">
+        <div class="iconItem">
+            <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-fanjutuijian"></use>
+            </svg>
+            <span>每日推荐</span>
+        </div>
+        <div class="iconItem">
+            <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-airec"></use>
+            </svg>
+            <span>私人FM</span>
+        </div>
+        <div class="iconItem">
+            <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-gedan"></use>
+            </svg>
+            <span>歌单</span>
+        </div>
+        <div class="iconItem">
+            <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-paihangbang"></use>
+            </svg>
+            <span>排行榜</span>
+        </div>
     </div>
 </template>
 <script>
@@ -8,3 +31,25 @@ export default{
     name:"lonlist",
 }
 </script>
+
+<style lang="less" scoped>
+.iconList{
+    width: 7.5rem;
+    display: flex;
+    justify-content: space-between;
+    padding: 0.4rem;
+    .iconItem{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        .icon{
+            width: 1.2rem;
+            height: 1.2rem;
+        }
+        span{
+            font-size: 0.26rem;
+        }
+    }
+}
+
+</style>
